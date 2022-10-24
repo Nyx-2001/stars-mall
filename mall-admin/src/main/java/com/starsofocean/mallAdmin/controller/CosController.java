744f1657ff076270d1e9b816938e4d2a93136a6d
starsofocean <2865143424@qq.com>
2022-10-24 23:50:52 +0800

    studying...

---
+++ mall-admin/src/main/java/com/starsofocean/mallAdmin/controller/CosController.java
@@ -0,0 +1,13 @@
+package com.starsofocean.mallAdmin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author starsofocean
+ * date 2022/10/22 23:46
+ */
+@RestController
+@RequestMapping("/qcloud/cos")
+public class CosController {
+}
---
+++ mall-admin/src/main/java/com/starsofocean/mallAdmin/controller/MinioController.java
@@ -0,0 +1,13 @@
+package com.starsofocean.mallAdmin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author starsofocean
+ * date 2022/10/22 23:59
+ */
+@RestController
+@RequestMapping("/minio")
+public class MinioController {
+}
